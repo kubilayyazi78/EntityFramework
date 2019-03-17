df4a78ad6ccdd3d10fab0f898c7b21388c80bbd0
kubilay yazı <kubilayyazi@hotmail.com>
2019-03-17 03:17:11 +0300

    CodeFirst uygulaması yapıldı.

---
+++ CodeFirstApp/Migrations/201903170011138_createCategory.cs
@@ -0,0 +1,27 @@
+namespace CodeFirstApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class createCategory : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        CategoryId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Description = c.String(),
+                    })
+                .PrimaryKey(t => t.CategoryId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Categories");
+        }
+    }
+}
---
+++ CodeFirstApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstApp
+{
+
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+    public class MoviesContext:DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public MoviesContext():base("MoviesConStr")
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MoviesContext db = new MoviesContext();
+            var category = new Category()
+            {
+                 Description="Decs 1 ",
+                 Name="Category 1"
+            };
+            db.Categories.Add(category);
+            db.SaveChanges();
+
+        }
+    }
+}
