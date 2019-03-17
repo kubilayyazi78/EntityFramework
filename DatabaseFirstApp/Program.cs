931acde1615dec9793d983602bc2e4391b5647e0
kubilay yazı <kubilayyazi@hotmail.com>
2019-03-17 03:01:56 +0300

    EntityFrameWork Database First console uygulaması yapıldı.

---
+++ DatabaseFirstApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NorthwindEntities1 db = new NorthwindEntities1();
+            var categories = db.Categories;
+            foreach (var cat in categories)
+            {
+                Console.WriteLine(cat.CategoryName);
+            }
+            Console.ReadKey();
+        }
+    }
+}
