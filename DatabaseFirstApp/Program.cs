80f1a16846529c5a4ee8c8d386442eb999db45fc
kubilay yazı <kubilayyazi@hotmail.com>
2019-03-17 04:17:38 +0300

    Modele stored procedure eklendi.

--- DatabaseFirstApp/Program.cs
+++ DatabaseFirstApp/Program.cs
@@ -11,10 +11,20 @@ namespace DatabaseFirstApp
         static void Main(string[] args)
         {
             NorthwindEntities1 db = new NorthwindEntities1();
-            var categories = db.Categories;
-            foreach (var cat in categories)
+            //var products = db.Ten_Most_Expensive_Products();
+            //foreach (var pro in products)
+            //{
+            //    Console.WriteLine(pro.TenMostExpensiveProducts + "/" + pro.UnitPrice);
+            //}
+            //var categories = db.Categories;
+            //foreach (var cat in categories)
+            //{
+            //    Console.WriteLine(cat.CategoryName);
+            //}
+            var sales = db.Sales_by_Year(new DateTime(1997, 1, 1), DateTime.Now);
+            foreach (var sale in sales)
             {
-                Console.WriteLine(cat.CategoryName);
+                Console.WriteLine(sale.Year+"/"+sale.Subtotal);
             }
             Console.ReadKey();
         }
